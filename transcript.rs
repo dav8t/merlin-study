fc5d6f03bc10e624ea0e20ff6625f2a5e7105840
0xgobinath <94050592+0xgobinath@users.noreply.github.com>
2023-10-11 12:27:23 +0530

    add: transcript.rs => function skeleton

---
+++ transcript.rs
@@ -0,0 +1,25 @@
+impl Transcript {
+    pub fn new() {}
+
+    pub fn append_message() {}
+
+    pub fn commit_bytes() {}
+
+    pub fn append_64() {}
+
+    pub fn commit_u64() {}
+
+    pub fn challenge_bytes() {}
+
+    pub fn build_rng() {}
+
+    pub struct TranscriptRngBuilder {}
+
+    impl TranscriptRngBuilder {
+        pub fn rekey_with_witness_bytes() {}
+
+        pub fn commit_witness_bytes() {}
+
+        pub fn finalize {}
+    }
+}
