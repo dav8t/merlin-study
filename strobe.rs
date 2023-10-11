3ae82ad4f99b9b16babdf2616077558bfed7d406
0xgobinath <94050592+0xgobinath@users.noreply.github.com>
2023-10-11 12:21:44 +0530

    add: strobe.rs => function skeleton

---
+++ strobe.rs
@@ -0,0 +1,19 @@
+impl Strobe128 {
+    pub fn meta_ad() {}
+
+    pub fn ad() {}
+
+    pub fn prf() {}
+
+    pub fn key() {}
+}
+
+impl Strobe128 {
+    fn run_f() {}
+
+    fn absorb() {}
+
+    fn overwrite() {}
+
+    fn squeeze() {}
+}
